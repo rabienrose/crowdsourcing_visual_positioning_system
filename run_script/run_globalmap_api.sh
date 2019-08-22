8f4aa15956845699b309659a6c812084cd7eff8f
Zili Wang <283136745@qq.com>
2019-08-22 11:13:08 +0800

    globalmap run script

--- run_script/run_globalmap_api.sh
+++ run_script/run_globalmap_api.sh
@@ -1,11 +1,11 @@
-ROOT_ADDR=/media/chamo/095d3ecf-bef8-469d-86a3-fe170aec49db/test_office
+ROOT_ADDR=/home/chamo/Documents/test_gps_merge
 MAP_ADDR=${ROOT_ADDR}/global
-BAG_ADDR=${ROOT_ADDR}/bag/08-19-10-47-28.bag
+BAG_ADDR=/home/chamo/Documents/data/buildnear10
 CONFIG_ADDR=${ROOT_ADDR}/config
 CACHE_ROOT=${ROOT_ADDR}/cache
 LOCAL_ROOT=${ROOT_ADDR}/local
 
-EXE_ROOT=/home/chamo/Documents/work/chamo_vps
+EXE_ROOT=/home/chamo/Documents/chamo_vps
 GLOBAL_MAP_ADDR=${EXE_ROOT}/devel/lib/global_map_api/global_map_api 
 
 #rm -rf ${CACHE_ROOT}
@@ -13,4 +13,11 @@ GLOBAL_MAP_ADDR=${EXE_ROOT}/devel/lib/global_map_api/global_map_api
 mkdir ${CACHE_ROOT}
 mkdir ${LOCAL_ROOT}
 mkdir ${MAP_ADDR}
-${GLOBAL_MAP_ADDR} --map_root=${MAP_ADDR} --bag_addr=${BAG_ADDR} --cache_root=${CACHE_ROOT} --config_root=${CONFIG_ADDR} --localmap_root=${LOCAL_ROOT}
+
+map_bag_list=`ls ${BAG_ADDR}`
+for map_bag_addr in ${map_bag_list}
+do
+    map_name=`basename "${map_bag_addr}"`
+    echo process bag: ${map_name}
+    ${GLOBAL_MAP_ADDR} --map_root=${MAP_ADDR} --bag_addr=${BAG_ADDR}/${map_bag_addr} --cache_root=${CACHE_ROOT} --config_root=${CONFIG_ADDR} --localmap_root=${LOCAL_ROOT}
+done
