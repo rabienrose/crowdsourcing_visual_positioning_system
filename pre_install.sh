de2a6545badedb038a169fd704e6fd06a295d04e
Zili Wang <283136745@qq.com>
2019-11-04 13:12:45 +0800

    sdaf

--- pre_install.sh
+++ pre_install.sh
@@ -1,3 +1,4 @@
-sudo apt-get install libglew-dev
+sudo apt-get install libglew-dev libdevil-dev libtbb2 libfreeimage-dev
 sudo apt-get install libdevil-dev
+sudo apt-get install libtbb2
 sudo apt-get install libfreeimage-dev
